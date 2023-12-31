4c20603eaf4a6eff5390b88a736ffbf2fe29c48a
Lumi Sampaio <lumi.sampaio@acrelec.com>
2023-12-31 16:31:22 -0300

    Fixing blog section

--- client/src/components/landing/sections/BlogSection.tsx
+++ client/src/components/landing/sections/BlogSection.tsx
@@ -1,12 +1,8 @@
-import {NextRouter, useRouter} from "next/router";
-import {motion} from "framer-motion";
+import { NextRouter, useRouter } from "next/router";
+import { motion } from "framer-motion";
 
-import BlogPost from "@/components/shared/BlogPost";
 import Button from "@/components/shared/Button";
-import {blogs} from "@/utils/mockData";
-import {shrinkVariant} from "@/utils/animations";
-
-import {BlogConfigType} from "@/types/config";
+import { shrinkVariant } from "@/utils/animations";
 
 const BlogSection = () => {
   const router: NextRouter = useRouter();
@@ -23,12 +19,6 @@ const BlogSection = () => {
         >
           LATEST BLOGS
         </motion.h1>
-
-        <div className="flex flex-wrap p-2 md:p-4">
-          {blogs.slice(0, 3).map((blog: BlogConfigType) => (
-            <BlogPost key={blog.id} {...blog} />
-          ))}
-        </div>
 
         <div className="flex justify-center p-8">
           <Button onClick={() => router.push("/blog")} className="w-48 p-3 text-xl">Read more</Button>
